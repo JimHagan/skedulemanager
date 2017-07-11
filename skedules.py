6b34ba6b61c06969af7fdfa7f7ea3efcb58b950e
Jim Hagan <jhagan@wayfair.com>
2017-07-11 13:23:55 -0400

    Small commit

---
+++ skedules.py
@@ -0,0 +1,11 @@
+"""
+Author: Jim Hagan
+Copyright 2017 All rights reserved.
+"""
+import pytz
+import requests
+from datetime import datetime, timedelta
+
+requests.adapters.DEFAULT_RETRIES = 3
+
+
