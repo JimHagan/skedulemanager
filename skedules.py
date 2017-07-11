bcc33838cfa0298648dc469971b74b7fdc6677f7
Jim Hagan <jhagan@wayfair.com>
2017-07-11 14:43:56 -0400

    More work on skedule object

--- skedules.py
+++ skedules.py
@@ -2,6 +2,7 @@
 Author: Jim Hagan
 Copyright 2017 All rights reserved.
 """
+import os
 import pytz
 import requests
 from datetime import datetime, timedelta
@@ -9,3 +10,16 @@ from datetime import datetime, timedelta
 requests.adapters.DEFAULT_RETRIES = 3
 
 
+class SkeduleManager(object):
+    def __init__(self)
+        self.username = username
+        self.pwd = pwd
+        self.endpoint = os.environ.get('SK_USERNAME', '')
+        self.endpoint = os.environ.get('SK_PWD', '')
+        self.endpoint = os.environ.get('SK_ENDPOINT', '')
+
+    def get_skedules(self):
+        pass
+
+    def add_or_update_skedule(self):
+        pass
