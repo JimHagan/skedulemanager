60e2c27537137bae288dc036285ac2510a1f9b53
Jim Hagan <jim.locately@gmail.com>
2017-07-12 01:16:44 +0000

    Latest code

--- skedules.py
+++ skedules.py
@@ -3,6 +3,7 @@ Author: Jim Hagan
 Copyright 2017 All rights reserved.
 """
 from config import ENV
+import json
 import os
 import pickle
 import pytz
@@ -14,8 +15,12 @@ requests.adapters.DEFAULT_RETRIES = 3
 
 class SkeduleManager(object):
     def _authenticate(self):
-        self.api_token = ''
-
+        signin_url = '{0}members/signin/'.format(self.endpoint) 
+	signin_data = {"username": self.username,"passwd": self.pwd}
+	response = requests.post(signin_url, data=json.dumps(signin_data))
+	self.api_token = response.json()['data']['token']
+        self.auth_headers = {"Authorization":"Bearer {0}".format(self.api_token)}
+	return response
     def __init__(self):
         self.username = ENV.get('SK_USERNAME', '')
         self.pwd = ENV.get('SK_PWD', '')
@@ -26,12 +31,16 @@ class SkeduleManager(object):
     def get_skedules(self):
         pass
 
-    def add_or_update_skedule(self):
-        pass
-
+    def post_new_skedule(self, skedule_content):
+        post_skedule_url = '{0}profiles/{1}/skedules/'.format(self.endpoint, self.username)
+        response =  requests.post(post_skedule_url, data=json.dumps(skedule_content), headers=self.auth_headers)        
+	print response
+	return response
 
 if __name__ == "__main__":
     skmgr = SkeduleManager()
     print skmgr.username
     print skmgr.pwd
-    print skmgr.endpoint    
+    print skmgr.endpoint
+    print skmgr.api_token 
+    skmgr.post_new_skedule({'title': 'PRO Sports Skedule'})   
